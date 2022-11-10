d15bdcaf8bcd465c8f91a6da52e73bc62f63f442
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-11-11 01:20:22 +0600

    all services data created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,11 +11,23 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zu49cyt.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v0q5o0h.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('excursion').collection('allservices');
 
-
+        app.get('/allservices', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices)
+        })
+    }
+    finally { }
+}
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('Running Port')
